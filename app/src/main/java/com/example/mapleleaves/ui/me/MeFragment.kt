d8d479017d2b95fed480cce9a346657fb6483ea4
Bamboo <1472902174@qq.com>
2021-08-24 17:16:30 +0800

    添加底部导航
    Signed-off-by: Bamboo <1472902174@qq.com>

---
+++ app/src/main/java/com/example/mapleleaves/ui/me/MeFragment.kt
@@ -0,0 +1,47 @@
+package com.example.mapleleaves.ui.me
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.example.mapleleaves.R
+import com.example.mapleleaves.databinding.FragmentHomeBinding
+import com.example.mapleleaves.databinding.FragmentMeBinding
+import com.example.mapleleaves.ui.home.HomeViewModel
+
+class MeFragment : Fragment() {
+
+    private lateinit var meViewModel: MeViewModel
+    private var _binding: FragmentMeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        meViewModel =
+            ViewModelProvider(this).get(MeViewModel::class.java)
+
+        _binding = FragmentMeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textMe
+        meViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
