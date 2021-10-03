0605448eae5cab851cba3a5e1e7d06d8b918d43a
Bamboo <1472902174@qq.com>
2021-10-03 20:24:25 +0800

    为教师的课程活动加载内容
    Signed-off-by: Bamboo <1472902174@qq.com>

--- app/src/main/java/com/example/mapleleaves/ui/course/TeacherCourseActivity.kt
+++ app/src/main/java/com/example/mapleleaves/ui/course/TeacherCourseActivity.kt
@@ -2,18 +2,28 @@ package com.example.mapleleaves.ui.course
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.mapleleaves.databinding.ActivityTeacherCourseBinding
 import com.example.mapleleaves.logic.model.CoursesAttendedResponse
+import com.example.mapleleaves.ui.home.TeachViewModel
 import com.example.mapleleaves.ui.signinsheet.SignInSheetActivity
 import com.example.mapleleaves.ui.startsignin.StartSignInActivity
 import com.example.mapleleaves.utils.LogUtil
+import com.example.mapleleaves.utils.MyObserver
 import com.google.gson.Gson
 
 class TeacherCourseActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityTeacherCourseBinding
+
+    private val TAG=this::class.java.simpleName
+
+    private val teachViewModel by lazy {ViewModelProviders.of(this).get(CourseViewModel::class.java)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +31,26 @@ class TeacherCourseActivity : AppCompatActivity() {
         binding= ActivityTeacherCourseBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Activity的布局会显示在状态栏上
+        val decorView=window.decorView
+        decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        // window.setDecorFitsSystemWindows(false)
+        //将状态栏设置成透明色
+        window.statusBarColor= Color.TRANSPARENT
+
         val jsonData=intent.getStringExtra("courseData")
         val courseData=Gson().fromJson(jsonData,CoursesAttendedResponse.Data::class.java)
+
+        teachViewModel.saveCourseData(courseData)
+        teachViewModel.courseLiveData.observe(this, Observer {
+            binding.apply {
+                tvCourseName.text=it.name
+                tvCourseCode.text=it.id
+                tvAddCourseCode.text=it.addClassCode
+                tvCourseMemberCount.text= it.number.toString()+"人"
+
+            }
+        })
 
         binding.ivCheckWork.setOnClickListener {
             StartSignInActivity.actionStart(this,courseData)
@@ -33,6 +61,7 @@ class TeacherCourseActivity : AppCompatActivity() {
             SignInSheetActivity.actionStart(this,courseData.id)
         }
 
+        lifecycle.addObserver(MyObserver(TAG))
     }
 
     companion object {
